5073cecd533fb6b792372a0edf3e9b9bfbf04629
apulache <apulache@gmail.com>
2023-05-14 23:13:09 -0500

    Dockerfile for Xavier-NX and updates

--- run_jupyterlab.sh
+++ run_jupyterlab.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 docker run -it --rm --runtime nvidia --network host \
--v $PWD:/workspace -w /workspace nvcr.io/nvidia/l4t-ml:r35.2.1-py3
+-v $PWD:/workspace -w /workspace apulache/l4t-transformers:v0.1-xavier-nx
+# nvcr.io/nvidia/l4t-ml:r35.2.1-py3
