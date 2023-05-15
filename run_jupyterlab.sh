1881ef829cb7396c1764e8ffd72a66fee546c738
apulache <apulache@gmail.com>
2023-05-14 22:55:42 -0500

    Setting workdir to workspace

--- run_jupyterlab.sh
+++ run_jupyterlab.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 docker run -it --rm --runtime nvidia --network host \
--v $PWD:/workspace nvcr.io/nvidia/l4t-ml:r35.2.1-py3
+-v $PWD:/workspace -w /workspace nvcr.io/nvidia/l4t-ml:r35.2.1-py3
